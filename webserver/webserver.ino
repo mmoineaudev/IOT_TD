21450f13a1af09ff06d99ca95800ccf5afe9cc60
Maxime M <maxime.moineau@free.fr>
2019-02-06 16:01:21 +0100

    starting to implement webserver

---
+++ webserver/webserver.ino
@@ -0,0 +1,97 @@
+#include <WiFi.h>
+
+WiFiServer server(80); //Declare un web server
+               
+void print_status(){
+  Serial.print("WiFi connected \n");
+  Serial.print("IP address: ");
+  Serial.print(WiFi.localIP());
+  Serial.print("\n");
+  Serial.print("MAC address: ");
+  Serial.print(WiFi.macAddress());
+  Serial.print("\n"); 
+}
+
+void connect_wifi(){
+ const char* ssid = "NetworkComesWithFaith"; 
+ const char* password= "0987654321"; 
+ 
+ Serial.println("Connecting Wifi...");
+ WiFi.begin(ssid, password);
+ while(WiFi.status() != WL_CONNECTED){
+   Serial.print("Attempting to connect ..");
+   delay(1000);
+ }
+ 
+ Serial.print("Connected to local Wifi\n");  
+ print_status();
+}
+
+// this method makes a HTTP reply:
+void httpReply(WiFiClient client) {
+   client.println("HTTP/1.1 200 OK");
+   client.println("Content-Type: text/html");
+   client.println("Connection: close");  // the connection will be closed after completion of the response
+   client.println("Refresh: 5");         // refresh the page automatically every 5 sec
+   client.println();
+   client.println("<!DOCTYPE HTML>");
+   client.println("<html>");    
+   printHTML(client)
+   client.println("</html>"); 
+}
+
+void printHTML(WiFiClient client){
+  int tempValue; 
+  client.print("<h2>température du batiment : ");
+  client.print(tempValue);
+  client.print(" degrés</h2>");
+  
+  int lightValue; 
+  client.print("<h2>luminosité du batiment : ");
+  client.print(lightValue);
+  client.print(" lumens</h2>");
+}
+
+void setup(){ 
+  Serial.begin(9600);
+  delay(1);
+
+  connect_wifi(); // Connexion Wifi  
+  server.begin(); // Lancement du serveur
+}
+
+void loop() {
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c); // Pour voir sur la console ce que l'on a recu
+       
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) { // send a standard http response header
+          httpReply(client);
+          break;
+        }
+        if (c == '\n') { // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') { // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    
+    // give the web browser time to receive the data
+    delay(100); // ms
+
+    // close the connection :
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
